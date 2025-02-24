31a7f2a066bdf712a5a9a4a9ef7089c138317735
Junting <junting.lu@gmail.com>
2025-02-24 01:55:11 +0000

    upload to the correct file!

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ You can view the deployed application This is a [here](https://goog-drive-clone.
 - [x] Add auth
 - [x] Add file uploading
 - [ ] Add analytics
+- [x] Add "ownership" to files and folders
+- [x] Upload files to the right folder
+- [ ] Delete files button
+- [ ] Allow files that aren't images to be uploaded
+- [ ] Real homepage
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,6 +4,7 @@ import {
   index,
   singlestoreTableCreator,
   bigint,
+  timestamp,
 } from "drizzle-orm/singlestore-core";
 
 /**
@@ -13,7 +14,7 @@ import {
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = singlestoreTableCreator(
-  (name) => `drive-tutorial_${name}`,
+  (name) => `drive_tutorial_${name}`,
 );
 
 export const files_table = createTable(
@@ -22,13 +23,19 @@ export const files_table = createTable(
     id: bigint("id", { mode: "number", unsigned: true })
       .primaryKey()
       .autoincrement(),
+    ownerId: text("owner_id").notNull(),
+
     name: text("name").notNull(),
     size: int("size").notNull(),
     url: text("url").notNull(),
     parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
   },
   (t) => {
-    return [index("parent_index").on(t.parent)];
+    return [
+      index("parent_index").on(t.parent),
+      index("owner_id_index").on(t.ownerId),
+    ];
   },
 );
 
@@ -40,11 +47,17 @@ export const folders_table = createTable(
     id: bigint("id", { mode: "number", unsigned: true })
       .primaryKey()
       .autoincrement(),
+    ownerId: text("owner_id").notNull(),
+
     name: text("name").notNull(),
     parent: bigint("parent", { mode: "number", unsigned: true }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
   },
   (t) => {
-    return [index("parent_index").on(t.parent)];
+    return [
+      index("parent_index").on(t.parent),
+      index("owner_id_index").on(t.ownerId),
+    ];
   },
 );
 
